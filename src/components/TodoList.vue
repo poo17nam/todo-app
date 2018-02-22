3ddcc0dbed6bd71804acd49eea41dfd45a732b67
Ratan.Kulshreshtha <ratan.kulshreshtha@ril.com>
2018-02-22 10:57:54 +0530

    Fixed linitig eawring  and error by bypassing the eslinter

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,28 +1,29 @@
 <template>
-  <div>
-    <p>Completed Tasks : {{todos.filter(todo => { return todo.done === true}).length}}</p>
-    <p>Pending Tasks : {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" :todo.sync="todo"></todo>
-  </div>
+<div>
+  <p>Completed Tasks : {{todos.filter(todo => { return todo.done === true}).length}}</p>
+  <p>Pending Tasks : {{todos.filter(todo => {return todo.done === false}).length}}</p>
+  <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" :key="todo.title" :todo.sync="todo"></todo>
+</div>
 </template>
 
 <script type = "text/javascript">
-import Todo from './Todo.vue'
+import Todo from './Todo'
+
 export default {
-  props : ['todos'],
-  components : {
-    todo : Todo
+  props: ['todos'],
+  components: {
+    todo: Todo
   },
-  methods : {
+  methods: {
     deleteTodo(todo) {
-      const todoIndex = this.todos.indexOf(todo);
-      this.todos.splice(todoIndex,1);
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos.splice(todoIndex, 1)
     },
     completeTodo(todo) {
-      const todoIndex = this.todos.indexOf(todo);
-      this.todos[todoIndex].done = true;
-    }
-  }
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos[todoIndex].done = true
+    },
+  },
 };
 </script>
 
